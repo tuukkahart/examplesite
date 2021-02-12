e2c6b93437d4eefafbcdf3f88b4d6916ef523b03
Tuukka Hartikainen <hartikainentuukka@gmail.com>
2021-02-12 23:37:31 +0200

    versio 1.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <h1>HotelHome</h1>
+                
             </div>
         </div>
     )
