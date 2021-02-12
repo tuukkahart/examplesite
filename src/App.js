b9c1630d5f2a8ddc1c206e48f8672f9c61acff6e
poseidoncom <hartikainentuukka@gmail.com>
2021-02-12 15:05:47 +0200

    esiversio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <h2>Portfolio...</h2>
+    <Navbar />
   );
 }
+
 
 export default App;
