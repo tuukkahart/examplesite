cdf312dd0dfe7575851289d1d9fc16ad7de837e0
emilia1998 <emilia.set@gmail.com>
2021-02-12 23:35:10 +0200

    navi update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ const Header = (props) => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <h1>HotelHome</h1>
+                    <h1>hotelhome</h1>
+                    
             </div>
         </div>
+        
     )
 }
 
